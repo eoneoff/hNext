287a338e95ea092fb5073c071b9128ae2cc06888
eoneoff <eoneoff@gmail.com>
2019-12-30 11:13:31 +0200

    migrated to latest .net version

---
+++ hNext/hNext.DataService/Controllers/RecordTemplateController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using hNext.IRepository;
+using hNext.Model;
+
+namespace hNext.DataService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RecordTemplateController : ControllerBase
+    {
+        private IRepository<RecordTemplate> _repository;
+
+        public RecordTemplateController(IRepository<RecordTemplate> repository) => _repository = repository;
+    }
+}
