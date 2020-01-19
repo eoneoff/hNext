cac53984932538f7fbdd888f8bda7eafb4b271d5
eoneoff <eoneoff@gmail.com>
2020-01-19 12:36:33 +0200

    record fields adding and removing

--- hNext/hNext.WebClient/wwwroot/js/record.js
+++ hNext/hNext.WebClient/wwwroot/js/record.js
@@ -25,6 +25,25 @@ Vue.component('Record', {
             }
         }
     },
+    methods: {
+        insertRecordRow: function (row) {
+            const field = {
+                orderNo: +row + 1,
+                value: ''
+            };
+            for (let i = +row + 1; i < this.record.recordFields.length; i++) {
+                this.record.recordFields[i].orderNo++;
+            }
+            this.record.recordFields.push(field);
+            this.record.recordFields.sort((f1, f2) => f1.orderNo - f2.orderNo);
+        },
+        deleteRecordRow: function (row) {
+            this.record.recordFields.splice(row, 1);
+            for (let i = +row; i < this.record.recordFields.length; i++) {
+                this.record.recordFields[i].orderNo--;
+            }
+        }
+    },
     mounted: function () {
         if (!this.initialRecord) {
             this.record.recordFields = this.recordTemplate.id
